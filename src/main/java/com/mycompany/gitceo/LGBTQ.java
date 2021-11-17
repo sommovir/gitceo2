30cd86cbc68e7445713cc0fc622c035c2733b76e
Luca Coraci <luca.coraci@istc.cnr.it>
2021-11-17 02:27:07 +0100

    recover deleted file

---
+++ src/main/java/com/mycompany/gitceo/LGBTQ.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gitceo;
+
+/**
+ *
+ * @author aless
+ */
+public class LGBTQ {
+    
+    private boolean sexixosos;
+    private int numeroMogli;
+    private int numeroMariti;
+    private String nome;
+    
+}
