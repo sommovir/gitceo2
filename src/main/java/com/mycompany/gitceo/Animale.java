306d66d7aece3c8b87875f05b48fd1af4ed401c4
SimoneMorolli <simone.morolli@gmail.com>
2021-11-17 21:59:07 +0100

    Minchia 10

--- src/main/java/com/mycompany/gitceo/Animale.java
+++ src/main/java/com/mycompany/gitceo/Animale.java
@@ -12,7 +12,7 @@ public class Animale {
  
     private String name;
     private String specie;
-    private int minchia = 15;
+    private int minchia = 10;
     
     public static void main(String[] args) {
         System.out.println(""+ (3/2));
