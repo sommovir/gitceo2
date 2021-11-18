bda3d56d596c1ff2e871a759efceec8d40365984
Luca Coraci <luca.coraci@gmail.com>
2021-11-18 04:35:09 +0100

    it surely works

--- src/main/java/com/mycompany/gitceo/GigaCalculator.java
+++ src/main/java/com/mycompany/gitceo/GigaCalculator.java
@@ -14,4 +14,17 @@ public class GigaCalculator {
         return a+b;
     }
     
+    /**
+     * it returns -1 if input is invalid
+     * @param message
+     * @return 
+     */
+    public static int getModules(String message){
+        if(message !=null && !message.isEmpty()){
+            String[] split = message.split(":");
+            return split.length;
+        }
+        return -1;
+    }
+    
 }
--- src/test/java/bisect/BugSolverTest.java
+++ src/test/java/bisect/BugSolverTest.java
@@ -17,36 +17,54 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author sommovir
  */
 public class BugSolverTest {
-    
+
     public BugSolverTest() {
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
-        
+
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
         System.out.println("OK");
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
-        
+
     }
-    
-    
+
     @Test
-    public void test1(){
+    public void test1() {
         int sum = GigaCalculator.sum(2, 3);
-        assertEquals(5, sum,"[FAIL]   ERRORE ENORME DEVASTANTE MAI VISTO");
+        assertEquals(5, sum, "     [Test 1][FAIL]   ERRORE ENORME DEVASTANTE MAI VISTO");
         System.out.println("====================================================\n");
-        System.out.println("     [SUCCESS]   Programma Perfetto! Complimenti Sir, lei è un ottimo programmatore.\n");
+        System.out.println("     [Test 1][SUCCESS]   Programma Perfetto! Complimenti Sir, lei è un ottimo programmatore.\n");
         System.out.println("====================================================\n");
+    }
+
+    @Test
+    public void test2() {
+        int m1 = GigaCalculator.getModules("minchi:niggi");
+        int m2 = GigaCalculator.getModules("minchi");
+        int m3 = GigaCalculator.getModules("minchi:niggi:polli");
+        int m4 = GigaCalculator.getModules("");
+        int m5 = GigaCalculator.getModules(null);
+        
+        assertEquals(2, m1, "     [Test 2][FAIL]   ERRORE ENORME DEVASTANTE MAI VISTO");
+        assertEquals(1, m2, "     [Test 2][FAIL]   ERRORE ENORME DEVASTANTE MAI VISTO");
+        assertEquals(3, m3, "     [Test 2][FAIL]   ERRORE ENORME DEVASTANTE MAI VISTO");
+        assertEquals(-1, m4, "     [Test 2][FAIL]   ERRORE ENORME DEVASTANTE MAI VISTO");
+        assertEquals(-1, m5, "     [Test 2][FAIL]   ERRORE ENORME DEVASTANTE MAI VISTO");
+        System.out.println("====================================================\n");
+        System.out.println("     [Test 2][SUCCESS]   Programma Perfetto! Complimenti Sir, lei è un ottimo programmatore.\n");
+        System.out.println("====================================================\n");
+
     }
 }
