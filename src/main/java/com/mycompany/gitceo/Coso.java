0c3ccb767e495a9ea5ead4dc0ef91674f00a5867
Luca Coraci <luca.coraci@gmail.com>
2021-11-18 04:47:39 +0100

    uti

--- src/main/java/com/mycompany/gitceo/Coso.java
+++ src/main/java/com/mycompany/gitceo/Coso.java
@@ -12,7 +12,7 @@ public class Coso {
     
     private int x;
     private String y = "la y è una bella consonante, anche la z";
-    private int sium = 1;
+    private int sium = 2;
 
     //qui si faranno cose
     
