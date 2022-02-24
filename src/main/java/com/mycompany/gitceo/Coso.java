87436469bd17d7a8224e006348c1a8c60d73ee0a
Luca Coraci <luca.coraci@gmail.com>
2022-02-24 13:11:34 +0100

    boh prova

--- src/main/java/com/mycompany/gitceo/Coso.java
+++ src/main/java/com/mycompany/gitceo/Coso.java
@@ -15,7 +15,7 @@ public class Coso {
     int h = -1;
     private int x = 0;
     private String y = "la K è una bella consonante";
-    private int zzz = 0;
+    private int zzz = 1;
 
     //SONO NEL MASTER
 
